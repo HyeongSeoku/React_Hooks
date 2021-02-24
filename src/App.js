f71f629b3ac9508e5124a92d4e00f7b3bac11502
gudtjr3437 <gudtjr3437@naver.com>
2021-02-24 16:01:13 +0900

    useTabs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+
+const content= [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  }
+];
+
+
 function App() {
-  const [item,setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const {currentItem,changeItem} = useTabs(0,content);
   return (
     <div>
-      <h2>{item}</h2>
-    <button onClick={incrementItem}>increment</button>
-    <button onClick={decrementItem}>decrement</button>
+      <h2>Hello</h2>
+      {content.map((section,index)=>(
+        <button onClick={()=>changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+
     </div>
   );
 }
---
+++ src/useTabs.js
@@ -0,0 +1,10 @@
+export const useTabs =(initalTab,allTabs) => {
+    const [currentIndex , setCurrentIndex] = useState(initalTab);
+    if(!allTabs || !Array.isArray(allTabs)){
+      return;
+    }
+    return {
+      currentItem : allTabs[currentIndex],
+      changeItem : setCurrentIndex
+    };
+  };
